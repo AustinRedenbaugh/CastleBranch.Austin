0d0ea8dbfcbbd8064d8453baf7dc88be1a9419b3
AustinRedenbaugh <aredenbaugh1@gmail.com>
2020-07-14 00:39:45 -0400

    nav and seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CountyAndCitiesTableSeeder::class);
+        $this->call([CountyAndCitiesTableSeeder::class, StatesTableSeeder::class);
     }
 }
---
+++ database/seeds/StatesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('states')->insert([
+            [
+                'name' => 'North Carolina',
+                'abbreviation' => 'NC'
+            ],[
+                'name' => 'California',
+                'abbreviation' => 'CA'
+            ]
+        ]);
+    }
+}
